c2830af3dd6d801aeb9488d3b4b84e29a2e77ea2
Adam Arbour <adam.arbour@gmail.com>
2026-02-23 05:35:40 +0000

    updates to hardware modules

--- modules/nixos/hardware/fprint.nix
+++ modules/nixos/hardware/fprint.nix
@@ -1,13 +1,25 @@
 { lib, pkgs, config, ...}:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) genAttrs mkEnableOption mkIf;
   c = config.sys.hw.fprint;
+  
+  pamServices = [
+    "login"
+    "sudo"
+    "greetd"
+    "sddm"
+    "polkit-1"
+    "swaylock"
+    "hyprlock"
+  ];
 in {
   options.sys.hw.fprint = {
     enable = mkEnableOption "enable fprint support";
   };
   
   config = mkIf (c.enable) {
-
+    services.fprintd.enable = true;
+    # Toggle fingerprint auth per selected PAM service
+    security.pam.services = genAttrs pamServices (_: { fprintAuth = true; });
   };
 }
--- modules/nixos/hardware/i2c.nix
+++ modules/nixos/hardware/i2c.nix
@@ -4,10 +4,17 @@ let
   c = config.sys.hw.i2c;
 in {
   options.sys.hw.i2c = {
-    enable = mkEnableOption "enable i2c support";
+    enable = mkEnableOption "enable i2c/ddcci support";
   };
   
   config = mkIf (c.enable) {
+    boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
+    boot.initrd.availableKernelModules = [ "ddcci-backlight" ];
+    
+    environment.systemPackages = with pkgs; [
+      ddccontrol ddccontrol-db
+    ];
+    
     hardware.i2c.enable = true;
   };
 }
