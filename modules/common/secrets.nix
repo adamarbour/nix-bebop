ecdd2eac50213b4358767564c1bc283f9f9ea08c
Adam Arbour <adam.arbour@gmail.com>
2026-02-24 15:46:33 +0000

    Prep for secrets

--- modules/common/secrets.nix
+++ modules/common/secrets.nix
@@ -1,12 +1,26 @@
-{ lib, config, _class, ... }:
+{ lib, config, sources, _class, ... }:
 let
-  inherit (lib) mkEnableOption;
+  inherit (lib) types mkOption mkEnableOption mkIf;
+  secretsRepo = sources.secrets;
+  c = config.sys.secrets;
 in {
   options.sys.secrets = {
     enable = mkEnableOption "enable sops-nix secrets...";
+    defaultSopsFile = mkOption {
+      type = types.path;
+      default = "${secretsRepo}/secrets/default.yaml";
+      readOnly = true;
+    };
+    hostSopsFile = mkOption {
+      type = types.path;
+      default = "${secretsRepo}/secrets/systems/${config.networking.hostName}.yaml";
+      readOnly = true;
+    };
   };
   
-  config = {
-
+  config = mkIf (c.enable) {
+    sops = {
+      inherit (c) defaultSopsFile;
+    };
   };
 }
---
+++ modules/nixos/secrets.nix
@@ -0,0 +1,16 @@
+{ lib, pkgs, config ... }:
+let
+  inherit (lib) mkIf;
+  s = config.sys.secrets;
+  root = if config.sys.impermanence.enable then
+    config.sys.impermanence.persistRoot
+    else null;
+in {
+  config = mkIf (s.enable) {
+    environment.systemPackages = with pkgs; [ age sops ssh-to-age ];
+    sops = {
+      age.sshKeyPaths = [ "${root}/etc/ssh/ssh_host_ed25519_key" ];
+      gnupg.sshKeyPaths = [];
+    };
+  };
+}
