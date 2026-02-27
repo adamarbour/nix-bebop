faef95b990b6b25be6f3f184a6f9c5986136786c
Adam Arbour <adam.arbour@gmail.com>
2026-02-27 16:36:14 +0000

    Baseline home manager

---
+++ homes/default.nix
@@ -0,0 +1,32 @@
+{ lib, config, sources, ... }:
+let
+  inherit (lib) genAttrs mkEnableOption mkIf;
+  c = config.hm;
+in {
+  options.hm = {
+    enable = mkEnableOption "enable the evaluation and loading of home manager modules";
+  };
+  
+  config = mkIf c.enable {
+    home-manager = {
+      useUserPackages = true;
+      useGlobalPkgs = true;
+      backupFileExtension = "old";
+      
+      users = genAttrs config.sys.users (name: {
+        imports = [ ./${name} ];
+      });
+      
+      extraSpecialArgs = {
+        inherit sources;
+        bebop = lib.bebop;
+        osConfig = config;
+      };
+      
+      sharedModules = [
+        ({ lib, bebop, ...}: { _module.args.lib = lib.extend (final: prev: { bebop = bebop; }); })
+        ../modules/home
+      ];
+    };
+  };
+}
