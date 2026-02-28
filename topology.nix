596035b82854d8fe41ff756e20f08ebf515812ab
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-02-28 16:55:24 -0600

    Prepping networks

--- topology.nix
+++ topology.nix
@@ -1,16 +1,18 @@
 { ... }:
 {
-  networks.home = {
-    name = "Home";
-    cidrv4 = "192.168.1.0/24";
+  networks.backplane = {
+    name = "wireguard";
+    style.pattern = "dotted";
+    cidrv4 = "10.12.34.0/24";
   };
-
-  # Example: declare a switch and connect hosts physically
-  nodes.core-switch = {
-    deviceType = "switch";
-    interfaces = {
-      swp1 = {};
-      swp2 = {};
-    };
+  networks.tailscale = {
+    name = "tailscale";
+    style.pattern = "dashed";
+    cidrv4 = "100.0.0.0/8";
+  };
+  networks.lab = {
+    name = "homelab";
+    style.pattern = "solid";
+    cidrv4 = "172.31.0.0/16";
   };
 }
