5c48b5e27bfe0609d11bc8d7127f9378e9405ece
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-02-28 15:52:00 -0600

    Didnt need the hack

--- modules/nixos/network/timezoned.nix
+++ modules/nixos/network/timezoned.nix
@@ -13,26 +13,5 @@ in {
     
     # https://github.com/NixOS/nixpkgs/issues/68489#issuecomment-1484030107
     services.geoclue2.enableDemoAgent = mkForce true;
-    
-    # handle roaming devices better
-    systemd.services.automatic-timezoned = {
-      serviceConfig = {
-        Type = mkForce "oneshot";
-        User = "automatic-timezoned";
-      };
-      after = [ "network.target" ];
-      wants = [ "network.target" ];
-    };
-    # run this as a timer instead...
-    systemd.timers.automatic-timezoned = {
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnBootSec = "2m";
-        OnUnitActiveSec = "15m";
-        RandomizedDelaySec = "1m";
-        Persistent = true;
-        Unit = "automatic-timezoned.service";
-      };
-    };
   };
 }
