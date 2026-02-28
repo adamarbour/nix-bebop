313edaedb7dce6ba325cd55ce132c0ad75f98e4d
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-02-28 06:27:23 +0000

    Nixos secrets

--- modules/nixos/secrets.nix
+++ modules/nixos/secrets.nix
@@ -1,6 +1,7 @@
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, config, sources, ... }:
 let
   inherit (lib) mkIf;
+  secretsRepo = sources.secrets;
   s = config.sys.secrets;
   root = if config.sys.persist.enable then config.sys.persist.storage.path else null;
   users = config.sys.users;
@@ -11,7 +12,18 @@ in {
       age.sshKeyPaths = [ "${root}/etc/ssh/ssh_host_ed25519_key" ];
       gnupg.sshKeyPaths = [];
       
-      
+      secrets = ( lib.foldl' ( acc: name:
+	      let
+          sopsFile = "${secretsRepo}/secrets/users/${name}.yaml";
+        in
+          acc // {
+            "users/${name}/passwd" = {
+              inherit sopsFile;
+              key = "passwd";
+              neededForUsers = true;
+            };
+          }) { }
+        users);
     };
   };
 }
