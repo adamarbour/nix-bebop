07293a39718fed8446aeebba8be7a6150531c61c
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-02-28 06:13:11 +0000

    ssh passwords handed out

--- modules/common/secrets.nix
+++ modules/common/secrets.nix
@@ -3,6 +3,8 @@ let
   inherit (lib) types mkOption mkEnableOption mkIf;
   secretsRepo = sources.secrets;
   c = config.sys.secrets;
+  users = config.sys.users;
+  home = if (_class == "darwin") then "/Users" else "/home";
 in {
   options.sys.secrets = {
     enable = mkEnableOption "enable sops-nix secrets...";
@@ -21,6 +23,31 @@ in {
   config = mkIf (c.enable) {
     sops = {
       inherit (c) defaultSopsFile;
+      
+      secrets = ( lib.foldl' ( acc: name:
+	      let
+          sopsFile = "${secretsRepo}/secrets/users/${name}.yaml";
+        in
+          acc // {
+            "users/${name}/id_ed25519" = {
+              inherit sopsFile;
+              key = "id_ed25519";
+              owner = name;
+              group = "users";
+              mode = "0600";
+              # materialize where the user expects it
+              path = "${home}/${name}/.ssh/id_ed25519";
+            };
+            "users/${name}/id_ed25519.pub" = {
+              inherit sopsFile;
+              key = "id_ed25519_pub";
+              owner = name;
+              group = "users";
+              mode = "0644";
+              path = "${home}/${name}/.ssh/id_ed25519.pub";
+            };
+          }) { }
+        users);
     };
   };
 }
