7f6086d2132e2325bd7243db95b02ac03c811594
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-02-28 20:42:37 +0000

    Better service handling

--- modules/nixos/network/timezoned.nix
+++ modules/nixos/network/timezoned.nix
@@ -10,5 +10,26 @@ in {
   config = mkIf (c.enable) {
     time.timeZone = mkForce null;
     services.automatic-timezoned.enable = true;
+    
+    # handle roaming devices better
+    systemd.services.automatic-timezoned = {
+      serviceConfig = {
+        Type = "oneshot";
+        User = "automatic-timezoned";
+      };
+      after = [ "network.target" ];
+      wants = [ "network.target" ];
+    };
+    # run this as a timer instead...
+    systemd.timers.automatic-timezoned = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "2m";
+        OnUnitActiveSec = "15m";
+        RandomizedDelaySec = "1m";
+        Persistent = true;
+        Unit = "automatic-timezoned.service";
+      };
+    };
   };
 }
