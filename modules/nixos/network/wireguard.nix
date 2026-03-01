0554e90bf06e54769e06fcb939b71a31556f1424
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-03-01 00:00:57 -0600

    initial wireguard

--- modules/nixos/network/wireguard.nix
+++ modules/nixos/network/wireguard.nix
@@ -1,39 +1,18 @@
 { lib, pkgs, config, ... }:
 let
-  inherit (lib) types mkOption mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf;
   c = config.sys.network.wireguard;
   s = config.sys.secrets;
 in {
   options.sys.network.wireguard = {
-    enable = mkEnableOption "enable wireguard";
-    role = mkOption {
-      type = types.enum [ "client" "hub" ];
-      default = "client";
-      description = "client dials a hub; hub accepts spokes and routes between them.";
-    };
-    
-    interface = mkOption {
-      type = types.str;
-      default = "wg0";
-    };
-    address = mkOption {
-      type = types.listOf types.str;
-      example = [ "10.44.0.2/32" ];
-    };
-    
-    listenPort = mkOption {
-      type = types.nullOr types.port;
-      default = 51820;
-    };
+    enable = mkEnableOption "enable wireguard support" // { default = true; };
   };
   
-  config = mkIf c.enable {
+  config = mkIf (c.enable) {
     boot.kernelModules = [ "wireguard" ];
     environment.systemPackages = [ pkgs.wireguard-tools ];
-    networking.firewall.allowedUDPPorts = lib.optional (c.listenPort != null) c.listenPort;
     
-    # expect wg from host secrets
-    sops.secrets = mkIf (s.enable) {
+    sops.secrets = mkIf s.enable {
       "host/wg.key" = {
         sopsFile = s.hostSopsFile;
         key = "wg/key";
@@ -42,11 +21,6 @@ in {
         sopsFile = s.hostSopsFile;
         key = "wg/pub";
       };
-    };
-    
-    networking.wg-quick.interfaces."${c.interface}" = {
-      inherit (c) address listenPort;
-      privateKeyFile = mkIf (s.enable) config.sops.secrets."host/wg.key".path;
     };
   };
 }
